706de46e93be7e9e2f96170c50e74b75bf7d816d
uaktags <timgarrity89@gmail.com>
2013-04-06 14:35:29 -0300

    Added Comments and fixed $tpl->assign placement

--- hooks/menu.php
+++ hooks/menu.php
@@ -24,18 +24,18 @@ function hook_menu_players($db, $tpl, $player, $args = 0) {
 	$menu .= "<li><a href='index.php?mod=AccountSettings'>Account</a></li>";
 	$menu .= "<li><a href='index.php?mod=Logout'>Log Out</a></li>";
 	*/
-	$query = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id IS NULL');
-	$results = $db->fetchAll($query);
-	foreach($results as $row => $val){
-	$query2 = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id = '. $val->id);
-	$rs = $db->fetchAll($query2);
-	$menu = "<ul>";
-	foreach($rs as $rw => $vl){
+	$query = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id IS NULL'); //Grab All Menu Groups
+	$results = $db->fetchAll($query); //Fetch All results
+	foreach($results as $row => $val){ //For Each Result, get Value
+	$query2 = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id = '. $val->id); //Grab Groups Children Menus
+	$rs = $db->fetchAll($query2); //Fetch All results
+	$menu = "<ul>"; //Start HTML list
+	foreach($rs as $rw => $vl){ //For each child get values
 	$menu .= "<li><a href='".$vl->uri."'>".$vl->title."</a></li>";
-	}
-	$menu .= "</ul>";
-	}
-	$tpl->assign('TOP_MENU_'. $val->title, $menu);
+	}// End of ForEach
+	$menu .= "</ul>";//End of Menu's List
+	$tpl->assign('TOP_MENU_'. $val->title, $menu); //Assign a Short Code for Template by the Group's Title
+	}// End of Group's ForEach
 	}
 	return $args;
 }
