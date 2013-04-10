b3a2c9a908d9f7d2e51782a695795e802cd94d24
uaktags <timgarrity89@gmail.com>
2013-04-09 22:46:42 -0300

    Update menu.php

--- hooks/menu.php
+++ hooks/menu.php
@@ -15,27 +15,7 @@ function hook_menu_players($db, $tpl, $player, $args = 0) {
 	$menu .= "</ul>";
   $tpl->assign('MENU_LOGGEDOUT', $menu);
 	} else {
-	/*
-	$menu = "<ul>";
-	$menu .= "<li><a href='index.php'>Home</a></li>";
-	$menu .= "<li><a href='index.php?mod=EventLog'>Log</a></li>";
-	$menu .= "<li><a href='index.php?mod=City'>City</a></li>";
-	$menu .= "<li><a href='index.php?mod=Members'>Members</a></li>";
-	$menu .= "<li><a href='index.php?mod=AccountSettings'>Account</a></li>";
-	$menu .= "<li><a href='index.php?mod=Logout'>Log Out</a></li>";
-	*/
-	$query = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id IS NULL'); //Grab All Menu Groups
-	$results = $db->fetchAll($query); //Fetch All results
-	foreach($results as $row => $val){ //For Each Result, get Value
-	$query2 = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id = '. $val->id); //Grab Groups Children Menus
-	$rs = $db->fetchAll($query2); //Fetch All results
-	$menu = "<ul>"; //Start HTML list
-	foreach($rs as $rw => $vl){ //For each child get values
-	$menu .= "<li><a href='".$vl->uri."'>".$vl->title."</a></li>";
-	}// End of ForEach
-	$menu .= "</ul>";//End of Menu's List
-	$tpl->assign('MENU_'. $val->title, $menu); //Assign a Short Code for Template by the Group's Title
-	}// End of Group's ForEach
+	get_menus($db, $tpl);
 	}
 	return $args;
 }
