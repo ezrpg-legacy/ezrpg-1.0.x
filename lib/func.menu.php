6a34729228502d3ec4f2f2c9e0f14d89af3391f2
uaktags <timgarrity89@gmail.com>
2013-04-10 22:20:44 -0300

    Add 2 new functions for Logouts and starting <ul>s

--- lib/func.menu.php
+++ lib/func.menu.php
@@ -21,11 +21,13 @@ defined('IN_EZRPG') or exit;
 
 
 */
-function get_menus(&$db, &$tpl)
+function get_menus(&$db, &$tpl, $menuname = "")
+{
+if($menuname == "")
 {
    if(LOGGED_IN != "TRUE") 
-  {
-  $menu = "<ul>";
+	{
+	$menu = "<ul>";
 	$menu .= "<li><a href='index.php'>Home</a></li>";
 	$menu .= "<li><a href='index.php?mod=Register'>Register</a></li>";
 	$menu .= "</ul>";
@@ -57,6 +59,44 @@ function get_menus(&$db, &$tpl)
 	}// End of Group's ForEach
 	
 	}
+}else{
+if(LOGGED_IN != "TRUE") 
+	{
+	$menu = "<ul>";
+	$menu .= "<li><a href='index.php'>Home</a></li>";
+	$menu .= "<li><a href='index.php?mod=Register'>Register</a></li>";
+	$menu .= "</ul>";
+  $tpl->assign('TOP_MENU_LOGGEDOUT', $menu);
+	} else {
+	$query = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE name = "'. $menuname. '"');
+	$result = $db->fetchAll($query);
+	$menu = "";
+	foreach ($result as $row => $val)
+	{
+		$menutag = "TOP_MENU_$val->name";
+		$query = $db->execute('SELECT * FROM `<ezrpg>menu` WHERE parent_id = '. $val->id);
+		$result = $db->fetchAll($query);
+		$menu .= get_menu_beginnings(); //Start HTML list
+		get_menu_beginnings();
+		foreach($result as $row => $val)
+			{
+				$menu .= "<li><a href='".$val->uri."'>".$val->title."</a>";
+				$query = $db->execute('SELECT COUNT(id) AS count FROM `<ezrpg>menu` WHERE parent_id = '. $val->id);
+				$result = $db->fetchAll($query);
+				if($result){ 
+					$menu .= get_menu_children($val->id, $db); 
+				} else {
+					$menu .= '';
+				}
+				$menu .= "</li>";
+			}// End of ForEach
+		$menu .= get_menu_endings();//End of Menu's List
+		$tpl->assign($menutag, $menu); //Assign a Short Code for Template by the Group's Title		
+	}// End of Group's ForEach
+	
+	}
+	}
+
 	return TRUE;
 }
 
@@ -83,5 +123,58 @@ function get_menu_children($id, &$db){
 			}// End of ForEach
 		$menu .= "</ul>";//End of Menu's List
 		return $menu;
+	}
+
+/*
+  Function: add_menu
+  Adds menu to database.
+
+  Returns:
+  Inserted ID of menu added
+  
+  Parameters:
+  $pid (Optional) Represents the Parent ID of the Menu this Menu belongs to.
+  &$db (Mandatory) Opens the function up to the Database class.
+  $name (Mandatory) Sets the 1word Name of the Menu.
+  $title (Mandatory) Sets the User-Friendly Name of the menu.
+  $uri (Optional) Sets the uri that the menu will go to.
+  
+  Example Usage:
+  $bid = add_menu('',$db,'Bank','Empire Bank', 'index.php?mod=Bank');
+  $add_menu ($bid, $db, 'Deposit', 'Deposit Money', 'index.php?mod=Bank&act=Deposit');
+*/
+	
+	function add_menu($pid = NULL, &$db, $name, $title, $uri = ''){
+		$item['parent_id'] = $pid;
+		$item['name'] = $name;
+		$item['title'] = $title;
+		$item['uri'] = $uri;
+		return $db->insert("<ezrpg>menu", $item);
+	}
+	
+/*
+  Function: add_menu_beginnings and add_menu_endings
+  Sets up the start and end of the menu.
+  
+  Parameters:
+  Dont Set the parameters
+  
+  Example Usage:
+  Only used in $get_menus
+*/
+	
+	function get_menu_beginnings($menu = ""){
+	$menu .= "<ul>"; //Start HTML list
+	$menu .= "<li><a href='index.php'>".(defined('IN_ADMIN')? "ADMIN" : "Home")."</a></li>";
+	return $menu;
+	}
+	function get_menu_endings($menu = "", $pre = ""){
+	if (defined('IN_ADMIN')){
+	$pre = "../";
+	$menu .= "<li><a href='../index.php'>To Game</a></li>";
+	}
+	$menu .= "<li><a href='".$pre."index.php?mod=" . (LOGGED_IN == 'TRUE' ? 'Logout' : 'Register'). "'>" .(LOGGED_IN == 'TRUE' ? 'Logout' : 'Register'). "</a></li>";
+	$menu .= "</ul>";
+	return $menu;
 	}
 ?>
