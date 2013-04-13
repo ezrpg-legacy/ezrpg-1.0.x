045374805a26f08f70045072eeb0c8ad5127f54f
uaktags <timgarrity89@gmail.com>
2013-04-13 00:23:14 -0300

    Update menu.php

--- hooks/menu.php
+++ hooks/menu.php
@@ -1,12 +1,4 @@
 <?php
-defined('IN_EZRPG') or exit;
-
-	$hooks->add_hook('player', 'menu_players');
-
-function hook_menu_players($db, $tpl, $player, $args = 0) {
-	
-	get_menus($db, $tpl);
-
-	return $args;
-}
+//REPLACED WITH CLASS.MENU.PHP
+//SEE LIB/CLASS.MENU.PHP
 ?>
